3e76f7ee10fecfa625b4c3f741da6daf19327f31
lsoyul <sgykim36@gmail.com>
2021-09-13 00:03:33 +0900

    * 변수 / 레지스터 개념 - bit byte word dword qword 개념 - 실행파일 section의 .data .bss 차이점 - 변수와 레지스터의 값 할당 및 전달방식

--- SASMProj/HelloWorld.asm
+++ SASMProj/HelloWorld.asm
@@ -3,23 +3,55 @@
 section .text
 global CMAIN
 CMAIN:
-    ;write your code here
+    mov rbp, rsp; for correct debugging
+
+    ; 8bit = 1byte
+    ; 16bit = 2byte = 1word
+    ; 32bit = 4byte = 2word = 1dword (double-word)
+    ; 64bit = 8byte = 4word = 1qword (quad-word)
     
-    ; 2진수 (0 1)
-    ; 0 1 10
-    ; 0b0 0b1 0b10 0b11 0b100
+    ; mov reg1, cst     (cst값을 reg1으로)
+    ; mov reg1, reg2    (reg2값을 reg1으로)
     
-    ; 16진수 (0 1 2 3 4 5 6 7 8 9 A B C D E F)
-    ; ~ F 10 11 12 ~
-    ; 0x00 
+    mov eax, 0x1234
+    mov rbx, 0x12345678
+    mov cl, 0xff
     
-    ; 0b 1001 0101 = 0x95 (2진수 <-> 16진수 변환 쉬움)
+    ;mov al, 0x00
+    ;mov rax, rdx
     
-    PRINT_STRING msg
+    ; 메모리 <-> 레지스터
+    ; mov rax, a    ; a라는 바구니의 메모리 주소값을 rax에 복사
+    ; mov rax, [a]    ; a라는 바구니 안에 있는 값을 rax에 복사
+    mov al, [a]
+    
+    mov [a], byte 0x55
+    mov [a], word 0x6666
+    mov [a], cl
     
     xor rax, rax
     ret
     
+    ; 변수의 선언 및 사용
+    ; - 처음 변수 사용 선언 (이름과 크기 지정)
+    
+    ; section .data 
+    ;-> 초기화 된 데이터
+    ; [변수이름] [크기] [초기값]
+    ; [크기] => db(1) dw(2) dd(4) dq(8)
 section .data
-    msg db 'Hello World', 0x00
+    a db 0x11       ; [0x11]
+    b dw 0x2222 
+    c dd 0x33333333
+    d dq 0x4444444444444444
+
+    ; section .bss 
+    ;-> 초기화 되지 않은 데이터
+    ;-> 모든 값들이 0으로 초기화
+    ;-> .data에 비해 실행파일[바이너리]의 크기가 커지지 않음 (값이 들어있지 않기 때문에)
+    ; [변수이름] [크기] [갯수]
+    ; [크기] resb(1) resw(2) resd(4) resq(8)
+section .bss
+    e resb 10
+    
     
