1371000e4b10e19e9f49a8a73aa4455b7c9658f2
lsoyul <sgykim36@gmail.com>
2021-09-22 16:33:30 +0900

    * bitwise 연산 개념

--- CPlus/CPP_Study/CPP_Study.cpp
+++ CPlus/CPP_Study/CPP_Study.cpp
@@ -1,83 +1,56 @@
 ﻿#include <iostream>
 using namespace std;
 
-// bool 은 그냥 정수 데이터지만, 참/거짓을 나타내기 위해 사용됨.
-// char도 마찬가지로 그냥 정수지만, '문자'의 의미를 나타내기 위해 사용.
-
-// char : 알파벳 / 숫자 문자를 나타낸다.
-// wchar_t : 유니코드 문자를 나타낸다.
-
-// ASCII (American Standard Code for Information Interchange)
-// a b c d e 1 2 3 4 5 ...
-char ch = 97; // => a
-// 문자의 의미로 '' 사용
-char ch1 = 'a';
-char ch2 = '1';
-char ch3 = 'a' + 1;
-
-
-// Global 시대에서 영어만으로 서비스 할 수 없음.
-// 전 세계 모든 문자에 대해 유일코드를 부여한 것이 유니코드 (unicode)
-// 참고) 유니코드에서 가장 많은 번호를 차지하는게 한국어/중국어 (뚫쿩웱.. 등 모든 문자를 표현해야하기 때문)
-
-
-// 유니코드는 표기 방식이 여러가지가 있는데, 대표적으로 UTF8 UTF16
-// UTF8
-// - 알파벳, 숫자는 1바이트 (ASCII 동일한 번호)
-// - 유럽 지역의 문자는 2바이트
-// - 한글, 한자 등은 3바이트
-// => 유동적임
-// UTF16
-// - 알파벳, 숫자, 한글, 한자 등 거의 대부분 문자는 2바이트
-// - 매~우 예외적인 고대 문자만 4바이트 (사실상 무시해도 됨)
-
-wchar_t wch = L'안';		// 앞에 L을 붙임
-wchar_t wch1 = 0xc548;
-
-// Escape Sequence
-// 표기하기 애매한 것들을 표현
-// \t = 아스키코드 9번 = Tab
-// \n = 아스키코드 10번 = LineFeed (한줄 아래로)
-// \r = 아스키코드 13번 = CarriageReturn (커서 맨 앞으로)
-// => enter 기능은 \r\n 으로 표현가능
-// \' = 기호 그 자체를 문자로 표현
-// \0 = null
-
-// 문자열
-// 문자들이 열을 지어서 모여있는 것 (문자 배열)
-// 정수 (1~8 바이트) 고정 길이로
-// \00 (null) 이 끝을 나타냄 (아스키코드 0번)
-
-char str[] = { 'h', 'e', 'l', 'l', 'o' };
-char str4[] = "Hello World";
-wchar_t str5[] = L"Hello World";
+unsigned char flag;	// 부호를 없애야 >> 를 하더라도 부호비트가 같아지지 않음
 
 int main()
 {
-	cout << ch << endl;
-	cout << ch2 << endl;
-	cout << ch3 << endl;
+#pragma region 비트 연산
+	// 언제 필요한가?
+	// 비트 단위의 조작이 필요 할 때,
 
-	// cout은 char 전용
-	cout << wch << endl; // X
+	// ~ bitwise NOT
+	// 단일 숫자의 모든 비트를 뒤집는다
 
-	wcout.imbue(locale("kor"));
-	wcout << wch << endl;
-	wcout << wch1 << endl;
+	// & bitwise AND
+	// 두 숫자의 모든 비트를 AND 연산 한다
 
-	// .data 영역에 저장되기 때문에 나머지 메모리영역이 0으로 초기화 되어있어 문제가 안된다.
-	cout << str << endl;
+	// | bitwise OR
+	// 두 숫자의 모든 비트를 OR 연산 한다
 
-	// Stack 영역에 저장이 되기 때문에 o 이후 쓰레기값들이 저장되어 있음.
-	char str2[] = { 'h', 'e', 'l', 'l', 'o' };
-	cout << str2 << endl;
+	// ^ bitwise XOR
+	// 두 숫자의 모든 비트를 XOR 연산 한다
+	// (암호학에서 유용하게 사용)
 
+	// << 비트 좌측 이동
+	// 비트열을 N만큼 왼쪽 으로 이동
+	// 이동하며 범위가 넘어서는 비트는 버린다. (새로 생성되는 비트는 0)
+	// *2를 할 때, 자주 보임 (컴파일러가 자주 사용하는 방식)
 
-	char str3[] = { 'h', 'e', 'l', 'l', 'o', '\0'};
-	cout << str3 << endl;
+	// >> 비트 우측 이동
+	// 비트열을 N만큼 오른쪽 으로 이동
+	// 이동하며 범위가 넘어서는 비트는 버린다.
+	// (*)왼쪽에서 새로 생성되는 비트는 그때그때 다름
+	// => 만약 부호 비트가 존재 할 시, 부호비트를 따라감. (부호있는 정수라면 이 부분을 유의)
+	// => 부호가 없다면 0이라고 생각하면 됨.
 
-	cout << str4 << endl;
-	wcout << str5 << endl;
+	// EX)
+	// 0b0000 [무적][변이][스턴][공중부양]
 
-	cout << 'a' << 'b' << 'c' << 'd' << '\n' << '\r' << 'e';
+	// 무적 상태로 만든다.
+	flag = (1 << 3);	// 8
+
+	// 변이 상태를 추가한다. (무적 + 변이)
+	flag |= (1 << 2);
+
+	// 무적인지 확인하고 싶다.
+	// bitmask
+	bool invincible = ((flag & (1 << 3)) != 0);
+
+	// 무적이거나 스턴상태인지 확인하고 싶다.
+	bool mask = (1 << 3) | (1 << 1);
+	bool stunAndInvincible = ((flag & 0b1010) != 0);
+
+#pragma endregion
+
 }
