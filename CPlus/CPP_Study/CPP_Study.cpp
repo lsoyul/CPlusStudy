bf3d2aaf394a8fe182e3cdb47eabd657e70f3a15
lsoyul <sgykim36@gmail.com>
2021-09-26 17:09:12 +0900

    * 다중/이중 포인터 개념

--- CPlus/CPP_Study/CPP_Study.cpp
+++ CPlus/CPP_Study/CPP_Study.cpp
@@ -1,53 +1,63 @@
 ﻿#include <iostream>
 using namespace std;
 
-// 포인터 vs 배열
+// 다중 포인터 (이중 포인터)
 
-void Test(int a)
+void SetNumber(int* a)
 {
-	a++;
+	*a = 1;
 }
 
-// 배열은 함수 인자로 넘기면, 컴파일러가 알아서 포인터로 치환한다. (char[] -> char*)
-// 즉 배열의 내용 전체를 넘긴게 아니라, 시작 주소(포인터)만 넘긴 것.
-void Test(char a[])
+void SetMessage(const char* a)
 {
-	a[0] = 'X';
-	cout << sizeof(a) << endl;
+	a = "Bye";
+}
+
+void SetMessage(const char** a)
+{
+	*a = "Bye";
+}
+
+void SetMessage2(const char*& a)
+{
+	a = "WOW";
 }
 
 int main()
 {
-	// .data 영역 주소[H][e][l]...
-	// test1 [주소] << 8바이트
-	const char* test1 = "Hello World";
-
-	// .data 영역 주소[H][e][l]...
-	// // => 4바이트씩 순차적으로 복사
-	// [][][][]...
-	// test2라는 바구니가 만들어진것이 아니지만, test2는 바구니 모음의 첫 주소를 가리킨다.
-	char test2[] = "Hello World";
-
-	// 포인터는 [주소를 담는 바구니]
-	// 배열은 그 자체로 같은 데이터끼리 붙어있는 '바구니들의 모음'
-	// - 다만 [배열 이름]은 '바구니 모음'의 '시작 주소'를 가리키고 있다.
-
-	cout << test1[0] << endl;
-	cout << test2[0] << endl;
-
-	cout << sizeof(test1) << endl;
-	cout << sizeof(test2) << endl;
-
-	// 배열을 함수의 인자로 넘겨주게 되면?
-
 	int a = 0;
-	// [매개변수][RET주소][지역변수][매개변수][RET주소][지역변수]
-	Test(a);
-	cout << a << endl;
+	SetNumber(&a);
 	
-	// test2가 바뀔까? 안바뀔까? -> 바뀐다!
-	Test(test2);	// 배열의 시작주소를 넘긴다.
-	cout << test2 << endl;
+
+	// .rodata Hello주소 - [H][e][l][l]...[\0]
+	// .rodata Bye주소 - [B][y][e][\0]
+	// 
+	// msg - [ 주소 ] << 8바이트
+	const char* msg = "Hello World";
+
+	// [매개변수][RET주소][지역변수(msg(Hello주소))] => SetMessage[매개변수(a(Hello주소))][RET주소][지역변수]
+	SetMessage(msg);
+	//cout << msg << endl;
+
+
+	// .rodata Hello주소 - [H][e][l][l]...[\0]
+	// msg [ Hello주소 ] << 8바이트
+	// pp [ &msg ] << 8바이트
+	const char** pp = &msg;
+
+	SetMessage(&msg);
+
+	cout << msg << endl;
+
+
+	// 다중포인터.. 혼동스럽다?
+	// 그냥 양파까기라고 생각하자.
+	// *을 하나씩 까면서 타고 간다고 생각
+	const char** pp2;
+
+	// 참조전달 버전
+	SetMessage2(msg);
+	cout << msg << endl;
 
 
 	return 0;
