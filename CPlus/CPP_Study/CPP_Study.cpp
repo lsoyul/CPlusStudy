e851c5b7db2ad2d8d50f4f80a8f65198bd025ad3
lsoyul <sgykim36@gmail.com>
2021-09-27 22:21:04 +0900

    * 문자열 간단 예제

--- CPlus/CPP_Study/CPP_Study.cpp
+++ CPlus/CPP_Study/CPP_Study.cpp
@@ -3,60 +3,55 @@ using namespace std;
 
 // 포인터 정리
 
-int* TestPointer()
+// 1) 문자열 길이 반환
+int StrLen(const char* str)
 {
-	int a = 1;
-	return &a;
+	// str이라는 문자열의 길이를 반환
+	char t = *str;
+	int length = 0;
+
+	while (t != '\0')
+	{
+		t = *(++str);
+		length++;
+	}
+
+	return length;
 }
 
-void TestWrong(int* ptr)
+// 2) 문자열 복사
+char* StrCopy(char* dest, const char* source)
 {
-	int a[100] = {};
-	a[99] = 0xAAAAAAAA;
-	*ptr = 0x12341234;
+	int cur = 0;
+
+	while (source[cur] != '\0')
+	{
+		dest[cur] = source[cur];
+		cur++;
+	}
+
+	dest[cur] = '\0';
+
+	return dest;
 }
+
 
 int main()
 {
-	// 2차원 배열과 다중포인터가 같다??
+	const int BUF_SIZE = 100;
 
-	// [1][2][3][4]
-	int arr[2][2] = { {1,2},{3,4} };
+	char a[BUF_SIZE] = "Hello";
+	char b[BUF_SIZE] = {};
 
-	// 00000001 [] << 4바이트 => error
-	// 주소1 [00000001]
-	// pp [주소1]
-	int** pp = (int**)arr;	// ?
+	//int len = sizeof(a);	// sizeof == a버퍼의 크기 == 100
+	//cout << len;
 
-	//cout << (**pp) << endl;
+	int len = StrLen(a);	// strlen == 실제 문자열의 크기
+	cout << a << endl;
 
-	// TYPE name[갯수]
-
-	// [1][2] [3][4]
-	// [ 주소 ]
-	int (*p2)[2] = arr;
-	cout << (*p2)[0] << endl;
-	cout << (*p2)[1] << endl;
-	cout << (*(p2 + 1))[0] << endl;
-	cout << (*(p2 + 1))[1] << endl;
-
-	cout << p2[0][0] << endl;
-	cout << p2[0][1] << endl;
-	cout << p2[1][0] << endl;
-	cout << p2[1][1] << endl;
-
-
-	// [매개변수][RET][지역변수] -TestPointer [매개변수][RET][지역변수]
-	// [매개변수][RET][지역변수]
-	// TestPointer가 리턴되면 스택 프레임에서 정리된다.
-	int* pointer = TestPointer();
-
-	TestWrong(pointer);
-	*pointer = 123;
-
-	cout << *pointer << endl;
-
-	// 스택프레임과 관련된 데이터의 주소값을 외부로 넘겨주지 않도록 하자!
+	cout << b << endl;
+	StrCopy(b, a);
+	cout << b;
 
 	return 0;
 }
