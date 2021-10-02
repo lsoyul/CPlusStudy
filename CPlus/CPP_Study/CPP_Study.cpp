d8466e0e310afa67ad248da85b59347e7ddba2f3
lsoyul <sgykim36@gmail.com>
2021-10-02 18:34:40 +0900

    * struct vs class 차이점  - static 변수/함수 개념

--- CPlus/CPP_Study/CPP_Study.cpp
+++ CPlus/CPP_Study/CPP_Study.cpp
@@ -1,150 +1,74 @@
 ﻿#include <iostream>
 using namespace std;
 
-// 연산자 오버로딩 (Operator Overloading)
+// 객체지향 마무리
 
-// 연산자 vs 함수
-// - 연산자는 피연산자의 갯수/타입이 고정되어 있음
+// 1) struct vs class
 
-// 연산자 오버로딩?
-// 일단 [연산자 함수]를 정의해야 함
-// 함수도 멤버함수 vs 전역함수가 존재하는 것 처럼, 연산자도 두가지 방식으로 만들 수 있음.
+// C++에서는 struct나 class나 종이 한 장 차이이다.
+// struct는 기본 접근 지정자가 public이고, class는 private 이다.
+// 왜 이렇게 했을까? C++은 C언어에서 파생되어 발전했기 때문에? (아마도)
+// -> struct는 그냥 구조체 (데이터 묶음)을 표현하는 용도로
+// -> class는 객체 지향 프로그래밍의 특징을 나타내는 용도
 
-// - 멤버 연산자 함수 version
-// -- a op b 형태에서 왼쪽을 기준으로 실행 (a가 특정 클래스여야 가능.)
-// -- a를 '기준 피연산자' 라고 함.
-// -- 한계) a가 클래스가 아니면 사용하지 못함
-
-// - 전역 연산자 함수 version
-// -- a op b 형태라면 a, b 모두를 연산자 함수의 피연산자로 만들어준다.
-
-
-// 그럼 무엇이 더 좋은건가? 그런거는 없다. 둘 중 하나만 지원하는 버전도 있음.
-// - 대표적으로 대입 연산자 (a = b)는 전역 연산자 version으로는 못만든다. (문법적으로 막아놓음)
-
-// 복사 대입 연산자
-// - 대입 연산자가 나온 김에 [복사 대입 연산자]에 대해 알아보자.
-// 용어가 좀 헷갈린다 [복사 생성자] [대입 연산자] [복사 대입 연산자]
-// - 복사 대입 연산자 = 대입 연사자인데 자기 자신의 참조타입을 인자로 받는 것.
-
-class Position
-{
-public:
-
-	//RET FUNC_NAME(ARG_LIST)
-	//{
-	//
-	//}
-	Position operator+(const Position& arg)
-	{
-		Position pos;
-		pos._x = _x + arg._x;
-		pos._y = _y + arg._y;
-
-		return pos;
-	}
-
-	Position operator+(int arg)
-	{
-		Position pos;
-		pos._x = _x + arg;
-		pos._y = _y + arg;
-
-		return pos;
-	}
-
-	bool operator==(const Position& arg)
-	{
-		return _x == arg._x && _y == arg._y;
-	}
-
-	Position& operator=(int arg)
-	{
-		_x = arg;
-		_y = arg;
-
-		//Position* this = 내 자신의 주소;
-		return *this;
-	}
-
-	// 복사 대입 연산자
-	// [복사 생성자] [복사 대입 연산자] 등이 특별히 대우를 받는 이유는
-	// 말 그래도 객체가 '복사'되길 원하는 특징 때문이다.
-	// => 컴파일러가 기본적으로 생성해주는 특징이 있음.
-	// TODO ) 동적할당 시간에 더 자세히 알아볼 것.
-
-	// 기타
-	// - 모든 연산자를 다 오버로딩 할 수 있는것은 아니다. (:: . .* 이런것은 안됨)
-	// - 모든 연산자가 다 2개 항이 있는 것이 아님. ++ -- 가 대표적 (단한 연산자)
-	// - 증감 연산자 ++ --
-	// -- 전위형 (++a) operator++()
-	// -- 후위형 (a++) operator++(int)
-
-	Position& operator=(const Position& arg)
-	{
-		_x = arg._x;
-		_y = arg._y;
-
-		//Position* this = 내 자신의 주소;
-		return *this;
-	}
-
-	// ++전위형
-	Position& operator++()
-	{
-		_x++;
-		_y++;
-	}
-
-	// 후위형++
-	Position operator++(int)
-	{
-		Position ret = *this;
-		_x++;
-		_y++;
-		return ret;
-	}
-
-public:
-	int _x;
-	int _y;
+struct TestStruct
+{// public:
+	int _a;
+	int _b;
 };
 
-// - 전역 연산자 버전
-Position operator+(int a, const Position& b)
+class TestClass
+{// private:
+	int _a;
+	int _b;
+};
+
+
+// 2) static 변수, static 함수 (static = 정적 = 고정된)
+
+class Marine
 {
-	Position ret;
-	ret._x = b._x + a;
-	ret._y = b._y + a;
+public:
+	// 특정 마린 객체에 종속적
+	int _hp;
 
-	return ret;
-}
+	void TakeDamage(int damage)
+	{
+		_hp -= damage;
+	}
 
+	static void SetAttack()
+	{
+		s_attack = 100;
+	}
+
+	// 특정 마린 객체와 무관
+	// 마린이라는 '클래스' 자체와 연관된 클래스 공통적인 값
+	static int s_attack;	// 설계상으로만 존재
+};
+
+// static 변수는 어떤 메모리를 사용?
+// 초기화를 하면 .data 영역
+// 초기화 안하면 .bss 영역
+// -> 어쨌든 메모리의 DATA 영역에 올라감
+
+int Marine::s_attack = 0;
 
 int main()
 {
-	int a = 1;
-	int b = 2;
-	int c = a + b;
+	Marine m1;
+	m1._hp = 40;
+	//m1.s_attack = 6;
 
-	Position pos;
-	pos._x = 0;
-	pos._y = 0;
+	Marine m2;
+	m2._hp = 40;
+	//m2.s_attack = 6;
 
-	Position pos2;
-	pos2._x = 1;
-	pos2._y = 1;
-
-	Position pos3 = pos + pos2;
-	// pos3 = pos.operator+(pos2);
-	Position pos4 = pos3 + 1;
-	// pos4 = pos.operator(1);
-	// Position pos4 = 1 + pos3; <= 멤버연산자로는 불가능
-	Position pos5 = 1 + pos3;
-
-	bool isSame = (pos3 == pos4);
-
-	pos5 = pos3++;
+	// 마린 공격력 업그레이드 완료! (Academy에서 업그레이드 끝)
+	Marine::s_attack = 6;
+	Marine::SetAttack();
+	//m1.s_attack = 7;
+	//m2.s_attack = 7;
 
 	return 0;
 }
