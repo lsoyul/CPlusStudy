d9797cf48fa1b4319f661302476d5eadd90670d7
lsoyul <sgykim36@gmail.com>
2021-10-03 22:45:38 +0900

    * 멤버 함수 포인터 구현 - typedef 동작 원리 - 멤버함수 포인터 사용법

--- CPlus/CPP_Study/CPP_Study.cpp
+++ CPlus/CPP_Study/CPP_Study.cpp
@@ -3,96 +3,95 @@ using namespace std;
 
 // 함수 포인터
 
-int Add(int a, int b)
+class Knight
 {
+public:
+
+	static void HelloKnight()
+	{
+
+	}
+
+	int GetHp(int, int)
+	{
+		cout << "GetHp()" << endl;
+		return _hp;
+	}
+
+public:
+	int _hp = 100;
+};
+
+
+// typedef의 진실
+// typedef 왼쪽 오른쪽 -> 오른쪽 (커스텀 타입 정의)
+
+//typedef int NUMBER;
+
+// 정확히는 왼쪽/오른쪽 기준이 아니라,
+// [선언 문법]에서 typedef를 앞에다 붙이는 쪽.
+
+typedef int INTEGER;
+typedef int* POINTER;
+typedef int FUNC(int, int);
+typedef int ARRAY[20];
+
+typedef int (*PFUNC)(int, int);					// 정적/전역 함수 포인터
+
+typedef int (Knight::*PMEMFUNC)(int, int);		// 멤버 함수 포인터
+
+int Test(int a, int b)
+{
+	cout << "Test" << endl;
 	return a + b;
 }
 
-int Sub(int a, int b)
-{
-	return a - b;
-}
-
-class Item
-{
-public:
-	Item() : _itemId(0), _rarity(0), _ownerId(0)
-	{
-
-	}
-
-public:
-	int _itemId;	// 아이템을 구분하기 위함 ID
-	int _rarity;	// 희귀도
-	int _ownerId;	// 소지자 ID
-};
-
-typedef bool(ITEM_SELECTOR)(Item*, int);
-
-//Item* FindItem(Item items[], int itemCount, bool(*selector)(Item* item))
-Item* FindItem(Item items[], int itemCount, ITEM_SELECTOR* selector, int value)
-{
-
-
-	for (int i = 0; i < itemCount; i++)
-	{
-		Item* item = &items[i];
-		
-		if (selector(item, value)) return item;
-	}
-
-	return nullptr;
-}
-
-bool IsRareItem(Item* item, int value)
-{
-	return (item->_rarity >= value);
-}
-
-bool IsOwnerITem(Item* item, int ownerId)
-{
-	return (item->_ownerId == ownerId);
-}
 
 int main()
 {
-	int a = 10;
-
-	// 바구니 주소
-	// pointer[ 주소 ]	->	주소[	]
-
-	typedef int DATA;
-
+	// 함수 포인터
 	// 1) 포인터			*
-	// 2) 변수 이름		pointer
-	// 3) 데이터 타입	int
-	DATA* pointer = &a;
+	// 2) 변수의 이름	fn
+	// 3) 타입			함수
 
-	// 함수
-	//typedef int(FUNC_TYPE)(int a, int b);
-	using FUNC_TYPE = int(int a, int b);
-	using TEST = void();
+	//FUNC t;
+	//t(1, 2);	// 구현부 없는 함수 호출상황이랑 같음
 
-	// 1) 포인터			*
-	// 2) 변수 이름		fn
-	// 3) 데이터 타입	FUNC_TYPE 함수 int(int a, int b)
-	FUNC_TYPE* fn;
+	//int (*fn)(int, int);
+	//FUNC* fn;
+	PFUNC fn;
 
-	fn = Add;
+	//typedef int(FUNC_TYPE)(int, int);
+	//FUNC_TYPE* fn;
 
-	// 함수의 이름은 함수의 시작 주소 (배열과 유사)
-	int result = Add(1, 2);
-	cout << result << endl;
+	fn = &Test;
+	//fn = Test;	&생략 가능 (C언어 호환성 때문)
 
-	result = fn(1, 2);
-	cout << result << endl;
+	fn(1, 2);
 
-	result = (*fn)(1, 2);	// 함수 포인터는 *(접근 연산자) 가 붙어도 함수 주소!
-	cout << result << endl;
+	(*fn)(1, 2);
 
-	Item items[10] = {};
-	items[3]._rarity = 2;	// RARE
-	FindItem(items, 10, IsRareItem, 2);
+
+	// 위 문법으로 [전역(global) 함수 / 정적(static) 함수] 만 담을 수 있다. (호출 규약이 동일한 애들)
+	
+	Knight k1;
+	k1.GetHp(1, 1);
+
+	//fn = &Knight::GetHp;
+
+	PMEMFUNC mfn;
+	mfn = &Knight::GetHp;
+
+	(k1.*mfn)(1, 1);
+
+	// => 멤버 함수 포인터는 *, & 구분을 잘 해주어야 한다.
+
+	Knight* k2 = new Knight();
+
+	((*k2).*mfn)(1, 1);
+	(k2->*mfn)(1, 1);
+
+	delete k2;
 
 	return 0;
 }
