3739006ac68b7600b14ec83af9335fa066ae2d5f
lsoyul <sgykim36@gmail.com>
2021-10-04 21:43:51 +0900

    * STL vector - 동적 배열 vector 기초 개념 / 원리

--- CPlus/CPP_Study/CPP_Study.cpp
+++ CPlus/CPP_Study/CPP_Study.cpp
@@ -1,79 +1,64 @@
 ﻿#include <iostream>
 using namespace std;
+#include <vector>
 
-// 콜백 (Callback)
-
-class Item
-{
-public:
-
-public:
-	int _itemId = 0;
-	int _rarity = 0;
-	int _ownerId = 0;
-};
-
-class FindByOwnerId
-{
-public:
-	bool operator()(const Item* item)
-	{
-		return (item->_ownerId == _ownerId);
-	}
-
-public:
-	int _ownerId;
-};
-
-class FindByRarity
-{
-public:
-	bool operator()(const Item* item)
-	{
-		return (item->_rarity >= _rarity);
-	}
-
-public:
-	int _rarity;
-};
-
-template<typename T>
-Item* FindItem(Item items[], int itemCount, T selector)
-{
-	for (int i = 0; i < itemCount; i++)
-	{
-		Item* item = &items[i];
-		// TODO : 조건 체크
-
-		if (selector(item)) return item;
-	}
-
-	return nullptr;
-}
+// STL Vector
 
 int main()
 {
-	// 함수포인터 + 함수 객체 + 템플릿
-	// 콜백 (Callback) : 다시 호출하다? 역으로 호출하다?
+	// STL (Standard Template Library)
+	// C++ 프로그래밍을 할 때 필요한 자료구조/알고리즘들을
+	// 템플릿으로 제공하는 라이브러리
 
-	// 게임을 만들 때 이런 콜백의 개념이 자주 등장한다.
-	// ex) MoveTask 실습 등
+	// 컨테이너(Container) : 데이터를 저장하는 객체 (자료구조 Data Structure)
 
-	// 어떤 상황이 발생하면 -> 이 기능을 호출해줘
-	// ex) UI 스킬 버튼을 누르면 -> 스킬을 쓰는 함수를 호출
+	// vector (동적 배열)
+	// - vector의 동작 원리 (size / capacity)
+	// - 중간 삽입/삭제
+	// - 처음,끝 삽입/삭제
+	// - 임의 접근
 
-	Item items[10];
-	items[3]._ownerId = 100;
-	items[8]._rarity = 1;
 
-	FindByOwnerId functor1;
-	functor1._ownerId = 100;	// wand find
+	// - 동적 배열
+	// 매우 매우 중요한 개념 -> 어떤 마법을 부렸길래 배열을 '유동적으로' 사용한 것인가?
 
-	FindByRarity functor2;
-	functor2._rarity = 1;
+	// 1) (여유분을 두고) 메모리를 할당한다.
+	// 2) 여유분까지 꽉 찼으면, 메모리를 증설한다.
 
-	Item* item1 = FindItem(items, 10, functor1);
-	Item* item2 = FindItem(items, 10, functor2);
+	// Q1) 여유분은 얼만큼이 적당할까?
+	// Q2) 증설을 얼만큼 해야 할까?
+	// Q3) 증설될 때, 기존의 데이터를 어떻게 처리할까?
+
+
+	// [ 1 2 3 4 5 ][ 마음대로 건들 수 없는 영역 ]
+	// [ 1 2 3 4 5                             ]	<= 다른 영역에 새로 할당
+	vector<int> v(1000,0);
+	vector<int> v2 = v;
+
+	v.push_back(2);
+	v.push_back(2);
+	v.push_back(2);
+	v.push_back(2);
+	// [				]
+	v.resize(1000);
+	// size (실제 사용 데이터 갯수)
+	// 1 2 3 4 5 6 7 ...
+	
+	//v.reserve(1000);	// <= capacity 할당
+	// capacity (여유분을 포함한 용량 갯수)
+	// 1 2 3 4 6 9 13 19 28 42 63 ...
+	// => 대략 이전 capacity의 1.5배 ~ 2배
+
+	for (int i = 0; i < 1001; i++)
+	{
+		v.push_back(100);
+		cout << v.size() << " " << v.capacity() << endl;
+	}
+
+
+	v.clear();				// <= size는 0으로 capacity는 그대로
+	vector<int>().swap(v);	// <= 새로운 깡통벡터와 swap되므로 size,capacity 둘 다 0으로 초기화
+	cout << v.size() << " " << v.capacity() << endl;
 
 	return 0;
 }
